13d590f7d38743c62a506c1de23b84d4d636fb64
Rodolfo d'Ettorre <rodolfothegreat@hotmail.com>
2021-09-08 19:15:40 +1000

    Initial commit

---
+++ app/src/main/java/com/rde/android/decisionmaker/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.rde.android.decisionmaker
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.security.SecureRandom
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getSupportActionBar()?.hide();
+        setContentView(R.layout.activity_main)
+
+        btnAnswer.setOnClickListener { updateTextBox() }
+
+
+
+    }
+
+    fun rand(start: Int, end: Int): Int {
+        require(start <= end) { "Illegal Argument" }
+        Thread.sleep(150)
+        val random = SecureRandom()
+        random.setSeed(random.generateSeed(120))
+
+        return random.nextInt(end - start + 1) + start
+    }
+
+    fun updateTextBox()
+    {
+
+        handler.postDelayed({tvAnswer.text = ""}, 0)
+        tvAnswer.invalidate()
+        val dodo = arrayOf("Yes", "No", "Maybe", "\nAsk me\n  later")
+        val anIndex = rand(0, 3)
+
+        handler.postDelayed({tvAnswer.text = dodo[anIndex]}, 300)
+
+
+    }
+}
